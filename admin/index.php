ec2dc8ad0887d0ec68342b62ead1d131008e469a
titus <villiger.titus@bluewin.ch>
2018-12-04 10:53:25 +0100

    Admin login

--- admin/index.php
+++ admin/index.php
@@ -4,38 +4,80 @@
     session_start();
     session_regenerate_id();
 
+    $error = $message = $name = $password = "";
+
     $pageTitle = "Admin Panel";
     $basePath = "../";
     include ($basePath."includes/headObject.php");
+    require_once($basePath."includes/database.php");
 
     $head = new Head($pageTitle, $basePath);
-    $head->addMenuItem(true, "Pending squads", "squadValidate.php");
+    if (isset($_SESSION['admin'])) {
+        $head->addMenuItem(true, "Pending squads", "squadValidate.php");
+    }
 
     $head->display();
 
-    //include("includes/head.php");
-?>
+    //check admin login
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        try {
+            require_once($basePath."validations/name.php");
+            if (isset($_POST['username'])) {
+                $name = new Name($_POST['username']);
+            }
+            else {
+                $error .= "Enter your Playername.";
+            }
+            if (isset($_POST['password'])) {
+                $password = $_POST['password'];
+            }
+            else {
+                $error .= "Enter your password";
+            }
+        }
+        catch (InvalidArgumentException $e) {
+            $error .= $e->getMessage();
+        }
+        if (empty($error)) {
+            //verify login:
+            $query = "SELECT Admin.Password FROM Admin INNER JOIN Player ON Admin.Player_id = Player.id WHERE Player.username = :username";
+            $db = new Database();
+            $handler = $db->connect();
+            $stm = $handler->prepare($query);
+            $stm->bindParam(":username", $name);
+            if (!$stm->execute()) {
+                $error .= "We seem to have a problem with our database. Try again later.";
+            }
+            else {
+                if ($stm->rowCount() == 0) {
+                    $error .= "You entered an invalid username and / or password.";
+                }
+                else {
+                    $result = $stm->fetch(PDO::FETCH_ASSOC);
+                    if (password_verify($password, $result['Password'])) {
+                        $_SESSION['admin'] = $name;
+                        $message .= "You are now logged in.";
+                    }
+                    else {
+                        $error .= "You entered an invalid username and / or password.";
+                    }
+                }
+            }
+        }
+    }
 
-<h1 class="form-heading">Admin-Login</h1>
-<div class="login-form">
-<div class="main-div">
-    <div class="panel">
-    <p>Identify yourself, please!<br>
-    <i>This is a restricted area, and we WILL log your ip when you try to log in.</i>
-    </p>
-   </div>
-    <form id="Login">
-        <div class="form-group">
-            <input type="text" class="form-control" id="name" placeholder="Your playername">
-        </div>
-        <div class="form-group">
-            <input type="password" class="form-control" id="password" placeholder="Your Password">
-        </div>
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
-    </div>
-</div></div></div>
+    if (!empty($message)) {
+        echo "<div class=\"alert alert-success\" role=\"alert\">".$message."</div>";
+    }
+    if (!empty($error)) {
+        echo "<div class=\"alert alert-danger\" role=\"alert\">".$error."</div>";
+    }
 
-<?php
+
+    //TODO: Put content outside of these files
+    if (!(isset($_SESSION['admin']))) {
+        include("content/loginForm.html");
+    }
+
     include($basePath."includes/foot.php");
 ?>
